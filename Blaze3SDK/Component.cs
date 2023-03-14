5f80eb94fa44cd8d7b654c2083a9bfd3ed3790e3
Aim2Strike <intarsmat@gmail.com>
2023-03-14 20:35:23 +0200

    Reorder component enum

--- Blaze3SDK/Component.cs
+++ Blaze3SDK/Component.cs
@@ -2,28 +2,28 @@
 {
     public enum Component : ushort
     {
-        UtilComponent = 9,
         AuthenticationComponent = 1,
         ExampleComponent = 3,
-        UserSessions = 30722,
         GameManager = 4,
-        GameReportingComponent = 28,
+        RedirectorComponent = 5,
+        PlaygroupsComponent = 6,
         StatsComponent = 7,
+        UtilComponent = 9,
         CensusDataComponent = 10,
         ClubsComponent = 11,
         GameReportingLegacyComponent = 12,
         LeagueComponent = 13,
         MailComponent = 14,
         MessagingComponent = 15,
-        RedirectorComponent = 5,
-        PlaygroupsComponent = 6,
         LockerComponent = 20,
         RoomsComponent = 21,
         TournamentsComponent = 23,
         CommerceInfoComponent = 24,
         AssociationListsComponent = 25,
         GpsContentControllerComponent = 27,
+        GameReportingComponent = 28,
         DynamicInetFilterComponent = 2000,
         RspComponent = 2049,
+        UserSessions = 30722,
     }
 }
